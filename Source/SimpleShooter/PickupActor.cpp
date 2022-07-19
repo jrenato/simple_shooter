c8fb3d5bca3c1603d439fe327aba3a02723cb48e
karvalho <renato@karvalho.com.br>
2022-07-19 17:17:16 -0300

    Updated assets

--- Source/SimpleShooter/PickupActor.cpp
+++ Source/SimpleShooter/PickupActor.cpp
@@ -9,6 +9,11 @@ APickupActor::APickupActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Root);
 }
 
 // Called when the game starts or when spawned
