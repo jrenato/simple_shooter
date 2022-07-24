b92371502ec215127a3bcd5b6067efe2d4fe55e8
karvalho <renato@karvalho.com.br>
2022-07-24 16:12:47 -0300

    Commented out future token code

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -5,10 +5,22 @@
 #include "EngineUtils.h"
 #include "GameFramework/Controller.h"
 #include "ShooterAIController.h"
+#include "PickupActor.h"
 
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
     Super::PawnKilled(PawnKilled);
+
+    // int NumPickups = 0;
+    // for (APickupActor* Pickup : TActorRange<APickupActor>(GetWorld()))
+    // {
+    //     if (Pickup->GetPoints() > 0)
+    //     {
+    //         NumPickups += Pickup->GetPoints();
+    //     }
+    // }
+
+    // UE_LOG(LogTemp, Warning, TEXT("%d pickups left"), NumPickups);
 
     APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
     if (PlayerController != nullptr)
